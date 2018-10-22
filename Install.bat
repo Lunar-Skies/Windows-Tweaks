a6e56d7b9d1ec083135574da72e919fec7e27cc9
Sean Furze <seanfjr02@gmail.com>
2018-10-22 13:21:00 -0400

    Install Chocolatey

--- Install.bat
+++ Install.bat
@@ -1,1 +1,24 @@
+@echo off
+goto check_Permissions
 
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate.
+    )
+
+pause >nul
+
+:: Start The Install Script Here ::
+
+: Install Chocolatey
+cls
+color 0a
+echo Installing chocolatey, please wait...
+sleep 4
+title Installing Chocolatey
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
